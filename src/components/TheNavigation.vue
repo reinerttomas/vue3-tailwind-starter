ca3ef18a8737227daa849e37a69ab44ee44f632a
Tomáš Reinert <reinerttomas@gmail.com>
2022-10-18 17:14:52 +0200

    supabase & login, register view

---
+++ src/components/TheNavigation.vue
@@ -0,0 +1,38 @@
+<template>
+  <header class="bg-at-light-green text-white">
+    <nav
+      class="container py-5 px-4 flex flex-col gap-4 items-center sm:flex-row"
+    >
+      <div class="flex items-center gap-x-4">
+        <img class="w-14" src="../assets/images/dumbbell-light.png" />
+        <h1 class="text-lg">Active Tracker</h1>
+      </div>
+      <ul class="flex flex-1 justify-end gap-x-10">
+        <router-link :to="{ name: 'Home' }" class="cursor-pointer"
+          >Home</router-link
+        >
+        <router-link :to="{ name: '' }" class="cursor-pointer"
+          >Create</router-link
+        >
+        <router-link :to="{ name: 'Login' }" class="cursor-pointer"
+          >Login</router-link
+        >
+        <li class="cursor-pointer">Logout</li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  setup() {
+    // get user from store
+
+    // Setup ref to router
+
+    // Logout function
+
+    return {};
+  },
+};
+</script>
