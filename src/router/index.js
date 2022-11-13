c9b8ea476273e0a7c49a80a2a290c121fcd15723
Tomáš Reinert <reinerttomas@gmail.com>
2022-11-13 17:18:26 +0100

    page title

--- src/router/index.js
+++ src/router/index.js
@@ -8,28 +8,51 @@ const router = createRouter({
       path: '/',
       name: 'Home',
       component: HomeView,
+      meta: {
+        title: 'Home',
+      },
     },
     {
       path: '/login',
       name: 'Login',
       component: () => import('../views/LoginView.vue'),
+      meta: {
+        title: 'Login',
+      },
     },
     {
       path: '/register',
       name: 'Register',
       component: () => import('../views/RegisterView.vue'),
+      meta: {
+        title: 'Register',
+      },
     },
     {
       path: '/create',
       name: 'Create',
       component: () => import('../views/CreateView.vue'),
+      meta: {
+        title: 'Create',
+      },
     },
     {
       path: '/workout/:id',
       name: 'Workout',
       component: () => import('../views/WorkoutView.vue'),
+      meta: {
+        title: 'View',
+      },
     },
   ],
 });
+
+// Change document titles
+router.beforeEach((to, from, next) => {
+  document.title = `${to.meta.title} | Active Tracker`;
+  next();
+});
+
+// Route guard for auth routes
 
 export default router;
